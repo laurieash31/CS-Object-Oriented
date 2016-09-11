ead22dbeace2908f667b94abf5a2167cb8d6cf71
Laura Garcia <laura.garcia31@txstate.edu>
2016-09-11 15:09:29 -0500

    Update Car.java
    I added the "get???" functions so that the private variables can be used. Not sure if we need them or not, but if we do, see my comment notes in the code. I also added the ArrayList to hold the entire car record put together.

--- Car.java
+++ Car.java
@@ -5,13 +5,49 @@
  */
 package assignment1;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author lauragarcia/Austin Stamper
  */
 public class Car {
     
-    private String[] vin = new String [5]; //Double check this; vin is always 5
+    /*
+    I'm not sure if we need these or not. If we do, we need a "set???" 
+    functions for each one since they are private. 
+    
+    And then have a function that either combines all the pieces of the record
+    into a string, and then put the string in the ArrayList as one long string for 
+    each car record line entered by the user. 
+    */
+    public String getVIN(){
+        return vin;
+    }
+    
+    public Integer getYear(){
+        return year;
+    }
+    
+    public Integer getMileage(){
+        return mileage;
+    }
+    
+    public Float getPrice(){
+        return price;
+    }
+    
+    public String getMake(){
+        return make;
+    }
+    
+    public String getModel(){
+        return model;
+    }
+    
+    
+    ArrayList<String> record = new ArrayList<String>();
+    private String vin; //Double check this; vin is always 5
     private int year;
     private int mileage;
     private float price;
