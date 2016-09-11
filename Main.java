5d99b5cf2bad762948cac849514a505f47eb93b3
Laura Garcia <laura.garcia31@txstate.edu>
2016-09-11 13:49:04 -0500

    Update Main.java
    I added a couple of try-catch statements to catch the file IO errors and user input error if they try to enter a letter or decimal number. Also added the do-while loop for the menu in the previous commit.

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ package assignment1;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.util.InputMismatchException;
 
 /**
  *
@@ -50,49 +51,51 @@ public class Main {
             System.out.println("------------------------------------------------------------------------");
             System.out.print(">> ");
             
+            //Error checking for bad user input
+            try{
             
-            //Add a try-catch to handle the "InputMismatchException" when user enters wrong info
-            
-            
-            Scanner input = new Scanner(System.in);
+                Scanner input = new Scanner(System.in);
            
-            //User input is held in "int selection"
-            System.out.println("Your selection is: " );
-            int selection = input.nextInt();
-            //System.out.println(selection + "\n");
+                //User input is held in "int selection"
+                System.out.println("Your selection is: " );
+                int selection = input.nextInt();
+                //System.out.println(selection + "\n");
             
-            //int comparison wasn't working, converted to string
-            String selectstring = "" + selection;
+                //int comparison wasn't working, converted to string
+                String selectstring = "" + selection;
         
-            //All inputs are read successfully
-            //Can use for when each option is selected
-            switch (selectstring) {
-                case "1":
-                    System.out.println("You selected 1");
-                    break;        
-                case "2":
-                    System.out.println("You selected 2");
-                    break;
-                case "3":
-                    System.out.println("You selected 3");
-                    break;
-                case "4":
-                    System.out.println("You selected 4");
-                    break;
-                case "5":
-                    System.out.println("You selected 5");
-                    break;
-                case "6":
-                    System.out.println("You selected 6");
-                    quit = true;
-                    break;
-                case "7":
-                    System.out.println("You selected 7");
-                    break;
-                default:
-                    System.out.println("Your selection is unavailable");
-                    break;
-                }
+                //All inputs are read successfully
+                //Can use for when each option is selected
+                switch (selectstring) {
+                    case "1":
+                        System.out.println("You selected 1");
+                        break;        
+                    case "2":
+                        System.out.println("You selected 2");
+                        break;
+                    case "3":
+                        System.out.println("You selected 3");
+                        break;
+                    case "4":
+                        System.out.println("You selected 4");
+                        break;
+                    case "5":
+                        System.out.println("You selected 5");
+                        break;
+                    case "6":
+                        System.out.println("You selected 6");
+                        quit = true;
+                        break;
+                    case "7":
+                        System.out.println("You selected 7");
+                        break;
+                    default:
+                        System.out.println("Your selection is unavailable");
+                        break;
+                    }
+            } catch(InputMismatchException e) {
+                System.out.println("You must enter a valid option!");
+            }                    
         } while(quit == false);
     }
 }
