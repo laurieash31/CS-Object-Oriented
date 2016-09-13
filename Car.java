9780af5c8aa59aaaa0e499503dfc02d76028984f
Laura Garcia <laura.garcia31@txstate.edu>
2016-09-12 20:05:34 -0500

    Update Car.java

--- Car.java
+++ Car.java
@@ -21,7 +21,11 @@ public class Car {
     into a string like on the slides from today, and then put the string in the ArrayList as one long string for 
     each car record line entered by the user.
     */
-        public Car(ArrayList<String> tempRecord, String tempVin, int tempYear, int tempMileage, float tempPrice, 
+    public Car(){
+            
+    }
+    
+    public Car(ArrayList<String> tempRecord, String tempVin, int tempYear, int tempMileage, float tempPrice, 
             String tempMake, String tempModel){
         this.record = tempRecord;        
         vin = tempVin;
