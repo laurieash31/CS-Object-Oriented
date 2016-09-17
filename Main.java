68be56ce02a0d90e156db4c00073a447eb8c7bbe
Laura Garcia <laura.garcia31@txstate.edu>
2016-09-17 16:29:44 -0500

    Update Main.java
    Updated main

--- Main.java
+++ Main.java
@@ -67,12 +67,10 @@ public class Main {
                     case "2":
                         System.out.println("You selected 2");
                         dealer.addCar();
-                        dealer.counter(count);
                         break;
                     case "3":
                         System.out.println("You selected 3");
                         dealer.deleteCar();
-                        dealer.counter(count);
                         break;
                     case "4":
                         System.out.println("You selected 4");
