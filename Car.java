4ca4373a6e64365cfa93476b2ebccfdc34f10b4d
Laura Garcia <laura.garcia31@txstate.edu>
2016-09-18 14:10:40 -0500

    Update Car.java
    Finalized Car.java

--- Car.java
+++ Car.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package assignment1;
 
 import java.util.ArrayList;
@@ -14,10 +9,11 @@ import java.util.Iterator;
  */
 public class Car {
     
-    public Car(){
-            
-    }
+    //This function is a default constructor
+    public Car(){}
     
+    
+    //This function is the constructor
     public Car(ArrayList<String> tempRecord, String tempVin, int tempYear, int tempMileage, float tempPrice, 
             String tempMake, String tempModel){
         this.record = tempRecord;        
@@ -29,65 +25,92 @@ public class Car {
         model = tempModel; 
     }
     
+    
+    //This function sets the newVIN to the vin in record
     public void setVIN(String newVIN){
         vin = newVIN;
     }
     
+    
+    //This function returns the vin from the record
     public String getVIN(){
         return vin;
     }
     
+    
+    //This function sets the year in record to newYear
     public void setYear(int newYear){
         year = newYear;
     }
     
+    
+    //This function returns the year from record
     public Integer getYear(){
         return year;
     }
     
+    
+    //This function sets the mileage in record from newMileage
     public void setMileage(int newMileage){
         mileage = newMileage;
     }
     
+    
+    //This function returns the mileage from record
     public Integer getMileage(){
         return mileage;
     }
     
+    
+    //This function sets the price in record to newPrice
     public void setPrice(float newPrice){
         price = newPrice;
     }
     
+    
+    //This function returns the price from record
     public Float getPrice(){
         return price;
     }
     
+    
+    //This function sets the make in record to newMake
     public void setMake(String newMake){
         make = newMake;
     }
     
+    
+    //This function returns the make from record
     public String getMake(){
         return make;
     }
     
+    
+    //This function sets the model in record to newModel
     public void setModel(String newModel){
         model = newModel;
     }
     
+    
+    //This function returns the model from record
     public String getModel(){
         return model;
     }
     
+    
+    //This function sets a record element to newRecord
     public void setRecord(String newRecord){
         record.add(newRecord);
     }
     
+    
+    //This function returns the record 
     public ArrayList<String> getRecord(){
         return this.record;
     }
     
-    //Removes an element from the ArrayList
+    //This function removes the element from record
     public void removeElement(String userVIN) {
-        //record.remove(element);
         //Find the matching car record VIN
         Iterator<String> iter = record.iterator();
 
@@ -96,12 +119,11 @@ public class Car {
 
             if (str.contains(userVIN) == true) {
                 iter.remove();
-            }
-        
+            } 
         }
     }
     
-    
+    //Variables
     private ArrayList<String> record = new ArrayList<String>();
     private String vin;
     private int year;
