bb6aa3ca4be8d74f3e17256d4d1f2e84412afebc
AustinStamper <austinstamper@gmail.com>
2016-09-27 20:34:01 -0500

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+/**
+ *
+ * @author Austin Stamper
+ */
+public class Customer extends User {
+    
+    public Customer(int dlNum, String pNum){
+        dlNumber = dlNum;
+        pNumber = pNum;
+    }
+    
+    public void setPNumber(String pNum){
+        pNumber = pNum;
+    }
+    
+    public String getPNumber(){
+        return pNumber;
+    }
+    
+    public void setDLNumber(int dlNum){
+        dlNumber = dlNum;
+    }
+    
+    public int getDLNumber(){
+        return dlNumber;
+    }
+    
+    private int dlNumber;
+    private String pNumber;
+}
