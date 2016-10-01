78c57f2d019f3a35ac04604dcceff253a161359e
Laura Garcia <laura.garcia31@txstate.edu>
2016-10-01 14:16:35 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package assignment2;
 
 /**
@@ -37,4 +32,3 @@ public class Customer extends User implements java.io.Serializable {
     private int dlNumber;
     private String pNumber;
 }
-
