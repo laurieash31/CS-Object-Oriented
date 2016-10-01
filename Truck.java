beab8c5eff45e75111a8df136dbed980047b8b6c
AustinStamper <austinstamper@gmail.com>
2016-10-01 18:45:17 -0500

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -4,27 +4,45 @@ package assignment2;
  * @author Austin Stamper
  * @author Laura Garcia
  */
+
+/** This class defines the variables and methods  unique to a Truck */
 public class Truck extends Vehicle implements java.io.Serializable{
     
+    /** Default Constructor */
     public Truck(){}
     
     public Truck(float weight, float len){
         maxLoadWeight = weight;
         length = len;
     }
-    
+    /**
+     * Sets the Max Load Weight
+     * @param weightthe weight to be set 
+     */
     public void setMaxLoadWeight(float weight){
         maxLoadWeight = weight;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public float getMaxLoadWeight(){
         return maxLoadWeight;
     }
     
+    /**
+     * Sets the Length
+     * @param len the length to be set
+     */
     public void setLength(float len){
         length = len;
     }
     
+    /**
+     * Returns the length
+     * @return the length
+     */
     public float getLength(){
         return length;
     }
