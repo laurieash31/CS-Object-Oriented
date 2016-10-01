0eee3eac82835084f63a9f94d54c8b5620c87f89
AustinStamper <austinstamper@gmail.com>
2016-10-01 18:42:52 -0500

    Update Motorcycle.java

--- Motorcycle.java
+++ Motorcycle.java
@@ -9,8 +9,11 @@ package assignment2;
  * @author Laura Garcia
  * @author Austin Stamper
  */
+
+/** This class defines the variables and methods unique to a Motorcycle */
 public class Motorcycle extends Vehicle implements java.io.Serializable{
     
+    /** Default Constructor */
     public Motorcycle(){}
     
     public Motorcycle(String type, int eng){
@@ -18,18 +21,34 @@ public class Motorcycle extends Vehicle implements java.io.Serializable{
         engine = eng;
     }
     
+    /**
+     * Sets the type of Motorcycle
+     * @param type the type of Motorcycle to be set
+     */
     public void setType(String type){
         motoType = type;
     }
     
+    /**
+     * Returns the type of Motorcycle
+     * @return the type of Motorcycle
+     */
     public String getType(){
         return motoType;
     }
     
+    /**
+     * Sets the engine CC
+     * @param eng the engine CC to be set
+     */
     public void setEngine(int eng){
         engine = eng;
     }
     
+    /**
+     * Returns the engine CC
+     * @return the engine CC
+     */
     public int getEngine(){
         return engine;
     }       
