94cd9f7e24e35a77d75e078de79c1006ca13da6b
AustinStamper <austinstamper@gmail.com>
2016-10-01 18:47:39 -0500

    Update User.java

--- User.java
+++ User.java
@@ -7,8 +7,11 @@ import java.util.Iterator;
  * @author Laura Garcia
  * @author Austin Stamper 
  */
+
+/** This class defines the methods and variables for all types of users */
 public class User implements java.io.Serializable {
     
+    /** Default Constructor */
     public User(){}
     
     public User(ArrayList<String> tempRecord, int id, String fName, String lName){
@@ -18,38 +21,74 @@ public class User implements java.io.Serializable {
         lastName = lName;
     }
     
+    /**
+     * Sets the User ID
+     * @param id 
+     */
     public void setID(int id){
         ID = id;
     }
     
+    /**
+     * Returns the user ID
+     * @return the User ID
+     */
     public int getID(){
         return ID;
     }
     
+    /**
+     * Sets the First Name
+     * @param fName the first name to be set
+     */
     public void setFirstName(String fName){
         firstName = fName;
     }
     
+    /**
+     * Returns the First Name
+     * @return the First Name
+     */
     public String getFirstName(){
         return firstName;
     }
     
+    /**
+     * Sets the Last Name
+     * @param lName the last name to be set
+     */
     public void setLastName(String lName){
         lastName = lName;
     }
     
+    /**
+     * Returns the Last Name
+     * @return the Last Name
+     */
     public String getLastName(){
         return lastName;
     }
     
+    /**
+     * Returns an item in the ArrayList
+     * @return 
+     */
     public ArrayList<String> getRecord(){
         return this.record;
     }
     
+    /**
+     * Adds a new item to the array list
+     * @param newRecord the item to be added to the array list
+     */
     public void setRecord(String newRecord){
         record.add(newRecord);
     }
     
+    /**
+     * Removes an item from the array list
+     * @param userID the user ID that is used to locate the item to be removed
+     */
     public void removeElement(String userID) {
         //Find the matching car record VIN
         Iterator<String> iter = record.iterator();
