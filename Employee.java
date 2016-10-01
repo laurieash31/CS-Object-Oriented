f8c2e3ed296fff61c5ea4c1086fdd59fa6746293
AustinStamper <austinstamper@gmail.com>
2016-10-01 18:41:58 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -4,8 +4,11 @@ package assignment2;
  * @author Laura Garcia
  * @author Austin Stamper
  */
+
+/** This class defines the variables and methods unique to an Employee */
 public class Employee extends User implements java.io.Serializable {
     
+    /** Default Constructor */
     public Employee(){}
     
     public Employee(float salary, int acctNum){
@@ -13,18 +16,34 @@ public class Employee extends User implements java.io.Serializable {
         accountNum = acctNum;
     }
     
+    /**
+     * Sets the Monthly Salary
+     * @param salary 
+     */
     public void setMonthSalary(float salary){
         monthSalary = salary;
     }
     
+    /**
+     * Returns the Monthly Salary
+     * @return the Monthly Salary
+     */
     public float getMonthSalary(){
         return monthSalary;
     }
     
+    /**
+     * Sets the Account Number
+     * @param acctNum 
+     */
     public void setAccountNum(int acctNum){
         accountNum = acctNum;
     }
     
+    /**
+     * Returns the Account Number
+     * @return the Account Number
+     */
     public int getAccountNum(){
         return accountNum;
     }
