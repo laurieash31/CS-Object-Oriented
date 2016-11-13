8749e4d88a7be040f93d49c4656724e9a49f750e
Laura Garcia <laura.garcia31@txstate.edu>
2016-11-13 12:35:46 -0600

    Update DealershipMenu.java
    Added the radio buttons for card2- User Management

--- DealershipMenu.java
+++ DealershipMenu.java
@@ -18,20 +18,26 @@ public class DealershipMenu implements ItemListener {
     
     //Names for each card
     final static String VEHICLEMANAGEMENTPANEL = "Vehicle Management";
-    final static String TEXTPANEL = "Card with JTextField";
+    final static String USERMANAGEMENTPANEL = "User Management";
+    final static String TRANSACTIONMANAGEMENT = "Transaction Management";
 
     
     public void addComponentToPane(Container pane) {
         
         //Put the JComboBox in a JPanel to switch between menus
         JPanel comboBoxPane = new JPanel(); //use FlowLayout
-        String comboBoxItems[] = { VEHICLEMANAGEMENTPANEL, TEXTPANEL };
+        String comboBoxItems[] = { VEHICLEMANAGEMENTPANEL, USERMANAGEMENTPANEL, TRANSACTIONMANAGEMENT };
         JComboBox cb = new JComboBox(comboBoxItems);
         cb.setEditable(false);
         cb.addItemListener(this);
         comboBoxPane.add(cb);
         
-        //Add buttons to button group
+       
+        
+        //Create the "cards" panels for Card 1 - Vehicle Management
+        JPanel card1 = new JPanel(new GridLayout(0,1));
+        
+        //Add buttons to button group for card1
         ButtonGroup radioButtonGroup = new ButtonGroup();
         JRadioButton displayButton = new JRadioButton("Display all vehicles");
         radioButtonGroup.add(displayButton);
@@ -44,23 +50,43 @@ public class DealershipMenu implements ItemListener {
         JRadioButton priceSearchButton = new JRadioButton("Search price range");
         radioButtonGroup.add(priceSearchButton);
         
-        //Add the action listeners to the radio buttons here later!!!!!!
+        //Add the action listeners to the radio buttons for card1 here later!!!!!!
         
-        //Create the "cards" panels
-        JPanel card1 = new JPanel(new GridLayout(0,1));
+        //Add the buttons to card1
         card1.add(displayButton);
         card1.add(addButton);
         card1.add(deleteButton);
         card1.add(searchButton);
         card1.add(priceSearchButton);
         
-        JPanel card2 = new JPanel();
-        card2.add(new JTextField("TextField", 20));
+        //Create the "cards" panels for Card 2 - User Management
+        JPanel card2 = new JPanel(new GridLayout(0,1));
+        //Add buttons to button group for card1
+        ButtonGroup radioButtonGroup2 = new ButtonGroup();
+        JRadioButton displayUsersButton = new JRadioButton("Display users");
+        radioButtonGroup2.add(displayUsersButton);
+        JRadioButton addUserButton = new JRadioButton("Add a user");
+        radioButtonGroup2.add(addUserButton);
+        JRadioButton updateUserButton = new JRadioButton("Update user information");
+        radioButtonGroup2.add(updateUserButton);
+        //card2.add(new JTextField("TextField", 20));
+        
+        //Add the action listeners to the radio buttons for card2 here later!!!!!!
+        
+        //Add the buttons to card1
+        card2.add(displayUsersButton);
+        card2.add(addUserButton);
+        card2.add(updateUserButton);
+        
+        //Create the "cards" panels for Card 3 - Transaction Management
+        JPanel card3 = new JPanel();
+        card3.add(new JTextField("TextField", 20));
         
         //Create the panel that contains the "cards".
         cards = new JPanel(new CardLayout());
         cards.add(card1, VEHICLEMANAGEMENTPANEL);
-        cards.add(card2, TEXTPANEL);
+        cards.add(card2, USERMANAGEMENTPANEL);
+        cards.add(card3, TRANSACTIONMANAGEMENT);
         
         pane.add(comboBoxPane, BorderLayout.PAGE_START);
         pane.add(cards, BorderLayout.CENTER);
