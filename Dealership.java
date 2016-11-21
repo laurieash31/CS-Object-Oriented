23192c58bdebca63233a0fdc3ffeeee73bfbcf48
Laura Garcia <laura.garcia31@txstate.edu>
2016-11-21 11:30:50 -0600

    Update Dealership.java
    This is only working IF the file has already been created. It seg faults or null pointer if the FileNotFoundException happens. All the commented out stuff was where I was trying to get the stuff to work.

--- Dealership.java
+++ Dealership.java
@@ -18,6 +18,7 @@ public class Dealership {
         
         //Create the GUI from the GUI class and set its properties
         DealershipMenuGUI frame = new DealershipMenuGUI();
+        frame.readDatabase();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -27,13 +28,19 @@ public class Dealership {
      * Main function to run on thread that displays the GUI window
     */
     public static void main(String[] args) {
-         
+        //DealershipMenuGUI frame = new DealershipMenuGUI();
+        //frame.readDatabase();
+        
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+                //DealershipMenuGUI frame = new DealershipMenuGUI();
+                //frame.readDatabase();
                 createDisplayGUI();
+                //frame.writeDatabase();
             }
         });
+        //frame.writeDatabase();
     }
 }
